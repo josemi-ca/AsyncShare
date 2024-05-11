efaf10c8a1818ed142746a9056c7a9c6cf478624
JMCA2 <802183+josemi-ca@users.noreply.github.com>
2024-05-11 16:09:21 +0200

    Añadir manifest + iconos PWA

--- resources/views/common_header.blade.php
+++ resources/views/common_header.blade.php
@@ -3,9 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon"
-          type="image/png"
-          href="/images/asyncshare_logo.png">
+    <link rel="icon" type="image/png" href="/images/asyncshare_logo.png">
+    <link rel="manifest" href="/manifest.json">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>AsyncShare</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
